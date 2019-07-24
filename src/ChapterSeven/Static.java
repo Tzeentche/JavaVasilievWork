1cce470cc21104433511319bf92f1e2c55d9cacf
Tzeentche <ilyasugako87@gmail.com>
2019-07-24 20:04:50 +0300

    Initial commit

---
+++ src/ChapterSeven/Static.java
@@ -0,0 +1,19 @@
+package ChapterSeven;
+
+import javax.swing.*;
+
+class FirstClass {
+
+    static int N1;
+    int N2;
+
+    FirstClass (int n1, int n2) {
+
+        N1 = n2;
+        N2 = n2;
+
+        String text = "Create new object! \n";
+    }
+}
+public class Static {
+}
