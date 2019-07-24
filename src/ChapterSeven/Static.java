9eeabf90fe62e1c35843ad2181d9f3f8344a5372
Tzeentche <ilyasugako87@gmail.com>
2019-07-24 20:28:23 +0300

    Started to developing decision for Static Methods by Vasiliev, Chapter 7

--- src/ChapterSeven/Static.java
+++ src/ChapterSeven/Static.java
@@ -13,7 +13,38 @@ class FirstClass {
         N2 = n2;
 
         String text = "Create new object! \n";
+        text += "Static field: " + N1 + "\n";
+        text += "Nonstatic field: " + N2;
+
+        JOptionPane.showMessageDialog(null, text);
+    }
+
+    void show() {
+
+        String text = "Object's fields! \n";
+        text += "Static field: " + N1 + " \n";
+        text += "Nonstatic field: " + N2;
+
+        JOptionPane.showMessageDialog(null, text);
     }
 }
+
 public class Static {
+
+    public static void main(String[] args) {
+
+        FirstClass A = new FirstClass(10, 200);
+        FirstClass.N1 = -50;
+
+        A.show();
+
+        FirstClass B = new FirstClass(1, 2);
+
+        A.show();
+
+        B.N1 = -1;
+        B.N2 = -2;
+
+        A.show();
+    }
 }
