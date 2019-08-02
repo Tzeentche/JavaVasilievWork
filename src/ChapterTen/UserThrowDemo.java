ec9c45533d5e5264c133e73333fc541c567cfe65
Tzeentche <ilyasugako87@gmail.com>
2019-08-02 21:33:29 +0300

    Finished to developing decision for User Exception by Vasiliev, Chapter 10

--- src/ChapterTen/UserThrowDemo.java
+++ src/ChapterTen/UserThrowDemo.java
@@ -15,7 +15,82 @@ class MyException extends Exception {
 
         String text = "Mistake has appear!\n";
         text += "Description: " + name + '\n';
+        text += "The number of mistake: " + count;
+
+        return text;
     }
 }
+
+class MyClassThrow {
+
+    private int number;
+
+    void show() {
+
+        System.out.println("Value of field: " + number);
+    }
+
+    MyClassThrow(int number) {
+
+        try {
+
+            if (number > 10) {
+
+                number = 10;
+
+                throw new MyException("It's too big number1");
+            }
+
+            if (number < 0) {
+
+                number = 0;
+
+                throw new MyException("It's a negative number!");
+            }
+        } catch (MyException obj) {
+
+            System.out.println(obj);
+        }
+
+        this.number = number;
+        show();
+    }
+
+    void set(int number) throws MyException {
+
+        if (number > 10 || number < 0) throw new MyException("Wrong argue!");
+        this.number = number;
+
+        System.out.println("All right!");
+        show();
+    }
+}
+
 public class UserThrowDemo {
+
+    public static void main(String[] args) {
+
+        MyClassThrow objA = new MyClassThrow(15);
+
+        MyClassThrow objB = new MyClassThrow(-1);
+
+        try {
+
+            objA.set(100);
+        } catch (MyException e) {
+
+            System.out.println(e);
+            objA.show();
+        }
+
+        MyException objE = new MyException("It's none-mistake!");
+
+        try {
+
+            throw objE;
+        } catch (MyException e) {
+
+            System.out.println(e);
+        }
+    }
 }
